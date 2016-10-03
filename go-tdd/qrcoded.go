e676e3775a07ece1944c83d63e5974a0f5868765
root <oscarvesga@gmail.com>
2016-10-03 22:03:02 +0000

    Refactor of code

--- go-tdd/qrcoded.go
+++ go-tdd/qrcoded.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"image"
+	"image/png"
+	"io"
+	"log"
+	"os"
 )
 
-func GenerateQRCode(code string) []byte {
-	return []byte{0xFF}
+// GenerateQRCode generate a QR Code with a phone number
+func GenerateQRCode(w io.Writer, code string) error {
+	img := image.NewNRGBA(image.Rect(0, 0, 21, 21))
+	return png.Encode(w, img)
 }
 
 func main() {
 	fmt.Println("Hello QR code")
-	
-	qrcode := GenerateQRCode("555-2368")
-	ioutil.WriteFile("qrcode.png", qrcode, 0644)
+
+	file, err := os.Create("qrcode.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	err = GenerateQRCode(file, "555-2368")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go-tdd/qrcoded_test.go
+++ go-tdd/qrcoded_test.go
@@ -1,28 +1,39 @@
 package main
 
 import (
-    "testing"
-    "bytes"
-    "image/png"
+	"bytes"
+	"errors"
+	"image/png"
+	"testing"
 )
 
-func TestGenerateQRCodeReturnValue (t *testing.T) {
-    result := GenerateQRCode("555-2368")
-    
-    if result == nil {
-        t.Errorf("Generated QRCode is nill")
-    }
-    if len(result) == 0 {
-        t.Errorf("Generated QRCode has no data")
-    }
+// Second Test
+func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
+	buffer := new(bytes.Buffer)
+	GenerateQRCode(buffer, "555-2368")
+
+	if buffer.Len() == 0 {
+		t.Errorf("No QRCode generated")
+	}
+
+	_, err := png.Decode(buffer)
+
+	if err != nil {
+		t.Errorf("Generated QRCode is not a PNG: %s", err)
+	}
 }
 
-func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
-    result := GenerateQRCode("555-2368")
-    buffer := bytes.NewBuffer(result)
-    _, err := png.Decode(buffer)
-    
-    if err != nil {
-        t.Errorf("Generated QRCode is not a PNG: %s", err)
-    }
+type ErrorWriter struct{}
+
+func (e *ErrorWriter) Write(b []byte) (int, error) {
+	return 0, errors.New("Expected error")
+}
+
+func TestGenerateQRCodePropagatesErrors(t *testing.T) {
+	w := new(ErrorWriter)
+	e := GenerateQRCode(w, "555-2368")
+
+	if e == nil || e.Error() != "Expected error" {
+		t.Errorf("Error not propagated correctly, go %v", e)
+	}
 }
